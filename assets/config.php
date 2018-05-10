d7b16be000fa69126e04e1b473102f2ad97d9a07
jaybailey <jabailey@vassar.edu>
2018-05-10 12:34:59 -0400

    Local Group Updates
    Updated the SASS file with the changes from the live version.  Compiled and tested the local-group.css file on cmspreview then published the updated file out to the live site.

---
+++ assets/config.php
@@ -0,0 +1,16 @@
+<?php
+return 
+array(
+// CMS site ID
+    'siteID' => '16064',
+	
+'layoutHeaderPost' => 'gallery:embed:homepage',
+	// Fonts
+	'typekitID' => 'qfv1enk',
+    
+        'siteNameHeader'=> '
+
+<b class="u-Masthead__siteName--line1">CAAD</b>
+<b class="u-Masthead__siteName--line2">Creative Arts Across Disciplines</b>',
+
+);
